07414c68df2ac357a4b8c9775778d6bfe1c620ca
Gman <matolagarnet@gmail.com>
2023-03-17 16:43:05 -0700

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *
+ *main - alphabet
+ *
+ *return - 0 (success)
+ *
+ */
+int main(void)
+{
+char alph[26] = "abcdefghijklmnopqrstuvwxyz";
+for (int i = 0; i < 26; i++)
+{
+putchar(alph[i])
+}
+putchar("\n");
+return 0;
+}

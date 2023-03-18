19aaae56527e9adbe26d03d8115b4ebc07c80837
Gman <matolagarnet@gmail.com>
2023-03-18 12:53:26 -0700

    Alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,11 +8,13 @@ int main(void)
 {
 char ualph[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 char lalph[26] = "abcdefghijklmnopqrstuvwxyz";
-for (int i = 0; i < 26; i++)
+int i;
+int j;
+for (i = 0; i < 26; i++)
 {
 putchar(lalph[i]);
 }
-for (int j = 0; j < 26; j++)
+for (j = 0; j < 26; j++)
 {
 putchar(ualph[j]);
 }

1b3a82cfaed5392087a4a6134ae49d4e2ccae469
Gman <matolagarnet@gmail.com>
2023-03-21 08:40:04 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/print_alph.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - for loop
+ */
+void print_alphabet(void)
+{
+int i;
+char alph[26] = "abcdefghijklmnopqrstuvwxyz";
+for (i = 0; i < 26; i++)
+{
+_putchar(alph[i]);
+}
+}

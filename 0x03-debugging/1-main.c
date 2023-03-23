aacb9e9b4aa6450147c8f1b648d836bfad4b66ef
Gman <matolagarnet@gmail.com>
2023-03-23 06:24:24 -0700

    debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,18 +7,13 @@
 
 int main(void)
 {
-        int i;
-
-        printf("Infinite loop incoming :(\n");
-
-        i = 0;
-
-        while (i < 10) /* i is not incremementing so it stays smaller than 10 thus loop doesnt stop */
-        {
-                putchar(i);
-        }
-
-        printf("Infinite loop avoided! \\o/\n");
-
-        return (0);
+int i;
+printf("Infinite loop incoming :(\n");
+i = 0;
+while (i < 10) /* i is not incremementing so it stays smaller than 10 thus loop doesnt stop */
+{
+putchar(i); /* keeps printing here */
+}
+printf("Infinite loop avoided! \\o/\n");
+return (0);
 }

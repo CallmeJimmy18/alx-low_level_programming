59ffc578a86749a7d5323b0536c0e6a8c685b681
Gman <matolagarnet@gmail.com>
2023-03-24 06:36:00 -0700

    Fizz Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - prints numbers 0 - 100
+ * Return: returns 0
+ */
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if ((i % 3) == 0)
+{
+printf("fizz");
+printf(" ");
+}
+else if ((i % 5) == 0)
+{
+printf("Buzz");
+printf(" ");
+}
+else
+{
+printf("%d", i);
+printf(" ");
+}
+}
+printf("\n");
+}

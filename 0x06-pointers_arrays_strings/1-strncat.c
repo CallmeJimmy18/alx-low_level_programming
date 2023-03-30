f7322d2f90b618c64e1f542efb79d4d876b0907e
Gman <matolagarnet@gmail.com>
2023-03-30 08:36:57 -0700

    concatinate n bytes

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strncat - concatinates n bytes of the string
+ * @dest: string will be appended here 
+ * @src: appended string
+ * @n: number of bytes
+ * Return: returns dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i;
+int j;
+while (dest[i] != '\0')
+{
+i++;
+}
+for (j = 0; j < n && src[j] != '\0'; j++)
+{
+dest[i] = src[j];
+i++;
+}
+dest[i] = '\0';
+return (dest);
+}

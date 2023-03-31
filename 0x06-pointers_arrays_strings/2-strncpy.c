8087b62da5620429e1861552f631bba98bbab6cf
Gman <matolagarnet@gmail.com>
2023-03-31 13:24:56 -0700

    Copied string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,11 +8,12 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-int i;
 int j;
-for (j = 0; j < n && src[j] != '\0'; j++)
+j = 0;
+while (j < n && src[j] != '\0')
 {
 dest[j] = src[j];
+j++;
 }
 while (j < n)
 {

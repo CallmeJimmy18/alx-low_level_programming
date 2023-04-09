e82630903410edde7c52c76e4d1e51d23cc89ec4
Gman <matolagarnet@gmail.com>
2023-04-09 15:03:03 -0700

    Name of programe

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints its name, followed by a new line.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}

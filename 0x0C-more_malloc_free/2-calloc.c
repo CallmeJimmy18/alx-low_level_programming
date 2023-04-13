c8d4591c461f4a67fa884734d10fa265e23511d5
Gman <matolagarnet@gmail.com>
2023-04-13 13:59:25 -0700

    array malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,28 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * _calloc - allocates memory for an array, using malloc
- * @nmemb: the array
- * @size: the number of bytes
- * Return: returns a pointer to the allocated space
- */
-char *_memset(char *s, char b, unsigned int n);
-void *_calloc(unsigned int nmemb, unsigned int size)
-{
-char *ptr;
-if (nmemb == 0 || size == 0)
-{
-return (NULL);
-}
-ptr = malloc(size * nmemb);
-if (ptr == NULL)
-{
-return (NULL);
-}
-_memset(ptr, 0, nmemb * size);
-return (ptr);
-}
-/**
  * *_memset - fills memory with a constant byte
  * @s: memory area to be filled
  * @b: char to copy
@@ -37,4 +15,26 @@ for (i = 0; i < n; i++)
 s[i] = b;
 }
 return (s);
+}
+
+/**
+ * _calloc - allocates memory for an array, using malloc
+ * @nmemb: the array
+ * @size: the number of bytes
+ * Return: returns a pointer to the allocated space
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+char *ptr;
+if (nmemb == 0 || size == 0)
+{
+return (NULL);
+}
+ptr = malloc(size * nmemb);
+if (ptr == NULL)
+{
+return (NULL);
+}
+_memset(ptr, 0, nmemb * size);
+return (ptr);
 }

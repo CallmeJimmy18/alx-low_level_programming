d293a3cf0baf73e0a5fd5d1435a6b7f111511940
Gman <matolagarnet@gmail.com>
2023-04-17 13:19:17 -0700

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - this is the dog
+ * @name: the dog name
+ * @age: the dogs age
+ * @owner: the dogs owner
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif

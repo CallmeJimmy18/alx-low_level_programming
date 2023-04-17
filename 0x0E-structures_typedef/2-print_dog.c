a95288c5954368eb8012cab957bc1eacaf7767db
Gman <matolagarnet@gmail.com>
2023-04-17 14:01:14 -0700

    printing struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -19,7 +19,7 @@ if (d->owner == NULL)
 {
 d->owner = "(nil)";
 }
- printf("Name: %s\n", d->name);
- printf("Age: %lf\n", d->age);
- printf("Owner: %s\n", d->owner);
+printf("Name: %s\n", d->name);
+printf("Age: %lf\n", d->age);
+printf("Owner: %s\n", d->owner);
 }

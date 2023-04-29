a785090b288ac84bb5faade4f808237180a12678
Garnet Matola <matolagernet@gmail.com>
2023-04-29 15:40:36 -0700

    add at end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -13,11 +13,11 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 list_t *nw;
 list_t *temp = *head;
-nw = malloc(sizeof(list_t));
 unsigned int len = 0;
 while (str[len])
 len++;
-if (nw == NULL)
+nw = malloc(sizeof(list_t));
+if (!new)
 {
 return (NULL);
 }

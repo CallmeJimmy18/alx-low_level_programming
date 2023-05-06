fcddc37c8da9d0f4b81724c54aa98d702de820a3
Garnet Matola <matolagernet@gmail.com>
2023-05-06 07:04:37 -0700

    clear

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+if (index > 63)
+{
+return (-1);
+}
+*n = (~(1UL << index) & *n);
+return (1);
+}

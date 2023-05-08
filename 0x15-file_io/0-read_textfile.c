41e8bcaa272b8abe40f563bd6dfd21bf4d646c1e
Garnet Matola <matolagernet@gmail.com>
2023-05-08 15:30:22 -0700

    read textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,8 +6,8 @@
 #include <fcntl.h>
 #include <unistd.h>
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output.
- * @filename - the name of the file
+ * read_textfile - reads a text file and prints it
+ * @filename: the name of the file
  * @letters: number of letters it should read and print
  * Return: actual number of letters it could read and print
  */
